e5f8d8401d4f23a8d12d8419dea69656eef024ad
Taina <tainnaps@gmail.com>
2021-09-29 17:18:30 -0300

    Conclui requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  // ref: http://frontendcollisionblog.com/javascript/2015/03/20/how-do-i-check-if-a-parameter-was-passed-in-to-a-javascript-function.html
+  if ((!name) && (!age)) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
