ccbcff888e0776da879ed334125ce62e47ade087
Arthur da Silva Rodrigues <arthur1silva1r@gmail.com>
2021-09-29 19:03:51 -0300

    requisito 5 -again and again-

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  return ({
-    name: nome,
-    feedback: () => { return 'Eita pessoa boa!'; }
-  });
-};
+const createStudent = (nome) => { return ({ name: nome, feedback: function() { return 'Eita pessoa boa!'; }}); };
 
 module.exports = createStudent;
