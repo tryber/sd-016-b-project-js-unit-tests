cd49c63949940b2bb189d291bfd5e02a72e8a6aa
Natã Elienai <nata.elienai@gmail.com>
2021-09-29 18:29:48 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,11 +17,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  let myArray = [];
+  for (let outerCounter = 0; outerCounter <= 3; outerCounter += 1) {
+    myArray.push(outerCounter);
+    for (let innerCounter = 2; innerCounter <= 3; innerCounter += 1) {
+      myArray.push(innerCounter);
     }
   }
   return myArray;
