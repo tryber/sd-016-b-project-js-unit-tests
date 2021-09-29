fe22ed91ef152821b2550a5551048c619ab844c4
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-29 17:17:10 -0300

    Req6-3: returns an array with correct length

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,6 +34,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se o retorno da função é um array.
     expect(productDetails('Alcool gel', 'Máscara')).toBeInstanceOf(Array);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('Alcool gel', 'Máscara')).toHaveLength(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
