e5f4dff1f06ad5ba22d0f494dacf0f6d70c9cdfe
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-09-29 17:48:43 -0300

    Requisito 5 Lint OK

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,7 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => ({ name: nome, feedback: () => 'Eita pessoa boa!' });
+console.log(createStudent('Carlin').feedback());
 
 module.exports = createStudent;
