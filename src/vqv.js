ecb1dbfb32bbedfafa0fae3cee0695c809a46989
Elton <eltonmorenocl@gmail.com>
2021-09-29 18:47:18 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (name === undefined) {
+    return undefined; 
+  }
+  if (name === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
