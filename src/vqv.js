9998ce740329c202287e9fb270d7c6ea4fa0790a
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-09-29 16:06:55 -0300

    requisito 03 concluido

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  let paragraph;
+  if (nome === undefined && idade === undefined) return undefined;
+
+  paragraph = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return paragraph;
+};
 
 module.exports = vqv;
