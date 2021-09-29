7e3eaa5be84feac3b61b577313ce4cbe12a115ee
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-09-29 15:31:14 -0300

    requisito 01 concluido

--- src/average.js
+++ src/average.js
@@ -12,6 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let total = 0;
+  let media = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+
+    total += array[index];
+  }
+
+  media = total / array.length;
+  return Math.round(media, 0);
+};
 
 module.exports = average;
