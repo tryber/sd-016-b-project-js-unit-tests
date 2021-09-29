2b9f225d931e340ec5a99213549df5368fbf49d1
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-29 15:55:05 -0300

    Req4.5: circle(2) ret. {.,circumference: 12.56,.}

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,6 +35,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     expect(circle()).toBeUndefined();
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    expect(circle(2).circumference).toBe(12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
