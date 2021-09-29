4e35f8e084a680eb377b25a1e614d929a5dd9f57
calidor <victor.calidor@gmail.com>
2021-09-29 17:57:38 -0300

    createStudent function

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const newObject = {
+    name: nome,
+    feedback() {
+    return 'Eita pessoa boa!';
+  } };
+  return newObject;
+};
 
 module.exports = createStudent;
