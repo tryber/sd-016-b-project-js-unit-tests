a3cd74e11ee7866ed15f0fb37638a77be8ddaf4f
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-29 20:53:28 -0300

    Req10-1: Step 1 && Test 1

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,5 +1,5 @@
 const createMenu = require('../src/restaurant');
- 
+
 /*
   Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
   - ler o menu cadastrado;
@@ -46,12 +46,14 @@ const createMenu = require('../src/restaurant');
 
 describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui a
     // chave fetchMenu, a qual tem como valor uma função.
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
+    expect(createMenu()).toBeInstanceOf(Object);
+    expect(createMenu()).toHaveProperty('fetchMenu');
+    expect(createMenu().fetchMenu).toBeInstanceOf(Function);
     // TESTE 2: Verifique se 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`, 
     // considerando que a função createMenu() foi chamada com o objeto: `{ food: {}, drink: {} }`.
     // ```
