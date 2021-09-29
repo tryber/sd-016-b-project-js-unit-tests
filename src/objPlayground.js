1bb6184130b545d639c2ec6cd079e6e019f31534
Matheus <matheusmoura2110@gmail.com>
2021-09-29 18:27:21 -0300

    task 8

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,10 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
 
-const arrayGenerator = (type, object) => {};
+};
+
+const arrayGenerator = (type, object) => { };
 
 module.exports = { calculator, arrayGenerator };
