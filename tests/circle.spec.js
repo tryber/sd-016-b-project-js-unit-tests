3f8200efc477ba859f149e9592465c5fff2b3012
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-09-29 18:24:27 -0300

    requisito: createStudent.js RESOLVIDO

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const circle = require('../src/circle');
+const circle = require('../src/circle')
 
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -25,30 +25,29 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-
     // ESCREVA SEUS TESTES ABAIXO:
 
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    expect(circle('a')).toBeUndefined();
+    expect(circle('a')).toBeUndefined()
 
     // Teste se circle retorna um objeto.
-    expect(typeof(circle(1))).toEqual('object');
+    expect(typeof circle(1)).toEqual('object')
 
     // Teste se o objeto retornado tem 3 propriedades.
-    expect(Object.keys(circle(1)).length).toEqual(3);
+    expect(Object.keys(circle(1)).length).toEqual(3)
 
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    expect(circle()).toBeUndefined();
+    expect(circle()).toBeUndefined()
 
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    expect(circle(2).circumference).toBe(12.56);
+    expect(circle(2).circumference).toBe(12.56)
 
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    expect(circle(3).area).toBecloseTo(28.26);
+    expect(Math.round(circle(3).area)).toBe(28)
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    expect(circle(3).radius).toEqual(3);
-    expect(circle(3).area).toBecloseTo(28.26, 2);
-    expect(circle(3).circumference).toBecloseTo(18.84);
-  });
-});
+    expect(circle(3).radius).toEqual(3)
+    expect(Math.round(circle(3).area)).toBe(28)
+    expect(circle(3).circumference).toBe(18.84)
+  })
+})
