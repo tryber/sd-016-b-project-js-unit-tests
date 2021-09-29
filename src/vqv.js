6b8d27e1cb1748d5e18536a6d449bc9e385866f3
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-29 15:21:23 -0300

    Req23.{1|2|3}: vqv is func. && ret. correct str.

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,9 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 
 module.exports = vqv;
