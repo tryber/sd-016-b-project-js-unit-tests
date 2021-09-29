38094df2726de599dd54d19f312c57ef6facd8c7
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-29 15:34:23 -0300

    Req4.2: circle(1) returns an object

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,6 +29,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
     expect(circle('a')).toBeUndefined();
     // Teste se circle retorna um objeto.
+    expect(circle(1)).toBeInstanceOf(Object);
     // Teste se o objeto retornado tem 3 propriedades.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
