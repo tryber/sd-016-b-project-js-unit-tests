68ce6dc64982d018e29149b088d9be58d82b1640
Gabriel Pinheiro <g.felippe5965@gmail.com>
2021-09-29 17:46:00 -0300

    Exercicio 5 - concluído

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (studentName) => {
+  const obj = {
+    name: studentName,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return obj;
+};
 
 module.exports = createStudent;
