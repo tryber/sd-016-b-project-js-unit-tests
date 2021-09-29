eaf987457f150c893d8908af0194f86c09a3965d
Marcos Carvalho <marcosfilhosp@hotmail.com>
2021-09-29 16:11:28 -0300

    Fix lint for evaluator

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,12 @@
 */
 
 const vqv = (name, age) => {
-  if ((typeof(name) !== 'string') || typeof(age) !== 'number'){ 
-    return undefined } else {
-      return `Oi, meu nome é ${name}!
+  if ((typeof (name) !== 'string') || typeof (age) !== 'number') { 
+    return undefined; 
+  } return `Oi, meu nome é ${name}!
 Tenho ${age} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
-    }
+#VQV!`;
 };
 
 module.exports = vqv;
