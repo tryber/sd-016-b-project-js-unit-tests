5d9bdea5c64c0c48cc55b782bf0280d44b4bf9a7
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-29 20:01:54 -0300

    Req9-1: getCharacter("Unknown") returns undefined

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -71,5 +71,6 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
       phrases: ['Not today', 'A girl has no name.']
     });
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Unknown')).toBeUndefined();
   });
 });
