bbb805b1253b1ef13099f7b777e99ec8b95bcc87
Matheus Machado <matheusmdec@hotmail.com>
2021-09-29 19:06:04 -0300

    Requisito 05 - Completo

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name, feedback) => {
+  const obj ={};
+  obj.name = name;
+  obj.feedback = () => {
+    return 'Eita pessoa boa!';
+  };
+  return obj
+};
 
 module.exports = createStudent;
