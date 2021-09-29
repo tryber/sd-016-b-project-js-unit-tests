90ed9af3531a0cc340681e1f2ab93ee6b636459f
morallys <morallys.juan@gmail.com>
2021-09-29 19:14:47 -0300

    Requisito 3 check

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,18 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+
+// console.log(vqv('Hamaji', 29));
 
 module.exports = vqv;
