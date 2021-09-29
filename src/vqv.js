a7dfd986fddea4045bda2a0dbb10d01cfc7efc11
morallys <morallys.juan@gmail.com>
2021-09-29 20:57:36 -0300

    Correção de digitação requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -21,7 +21,7 @@ const vqv = (name, age) => {
     return undefined;
   }
 
-  return `Oi, meu name é ${name}!
+  return `Oi, meu nome é ${name}!
 Tenho ${age} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
