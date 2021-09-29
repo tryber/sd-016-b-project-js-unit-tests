147286ed3b7f3a443e66970356d312d0fdd68213
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-09-29 19:55:24 -0300

    Requisito 6 Lint OK

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
+    const productFunc = productDetails('Alcool', 'Máscara');
     // Teste se productDetails é uma função.
+    expect(typeof(productDetails)).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productFunc)).toBe(true);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productFunc.length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof(productFunc[0])).toBe('object');
+    expect(typeof(productFunc[1])).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productFunc[0]).not.toEqual(productFunc[1]);
     // Teste se os dois productIds terminam com 123.
+    const firstId = productFunc[0].details.productId;
+    const secondId = productFunc[1].details.productId;
+    expect(firstId.substring(firstId.length - 3)).toBe('123');
+    expect(secondId.substring(secondId.length - 3)).toBe('123');
   });
 });
