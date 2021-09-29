92e79f159bddd8d1e8580671d33f55eada98fe19
Guilherme Rosa <gui.rsa@hotmail.com>
2021-09-29 17:15:15 -0300

    ex vqv

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,13 @@
 */
 
 const vqv = (string, number) => {
-  if (string.length === 0 && number.length === 0) {
+  if (string.length === 0 || number.length === 0) {
     return undefined;
   } 
-    let retorno = console.log(`Oi, meu nome é ${string}!
-    Tenho ${number} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`);
-    return retorno;
+  return `Oi, meu nome é ${string}!
+Tenho ${number.toString()} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
