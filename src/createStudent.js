bdbe49c007d4384df0cb0111a5b6c53980d7d755
igor <igor.rafael.briano@gmail.com>
2021-09-29 20:24:44 -0300

    finalizado requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,17 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const estudante = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  estudante.name = nome;
+  
+  console.log(estudante.feedback());
+  return estudante;
+};
+
+console.log(createStudent('Leandro'));
 
 module.exports = createStudent;
