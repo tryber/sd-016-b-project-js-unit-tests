ab68ec0c4074453a3b6039bd42367b4f99c4d002
Lucca Neiva <luccaneiva12@gmail.com>
2021-09-29 07:12:49 -0300

    Requisito 3 - concluido

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (string, number) => {
+  if (typeof string !== 'string' && typeof number !== 'number') return undefined;
+
+  return `Oi, meu nome é ${string}!
+Tenho ${number} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
