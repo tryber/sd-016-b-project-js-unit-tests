eaa3fa4cfaae80ad8c4027e19625331f757f2fd0
Fredi Wilkens <frediwilkens@gmail.com>
2021-09-29 15:58:14 -0300

    Ajuste no Requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -15,19 +15,17 @@ const numbers = require('../src/numbers');
 */
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função recebe [1, 2, 3, 4, 5] e retorna true', () => {
+  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
+    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
-  });
 
-  it('Verifica se a função recebe [1, 2, "3", 4, 5] e retorna false', () => {
+    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     expect(numbers([1, 2, '3', 4, 5])).toEqual(false);
-  });
 
-  it('Verifica se a função recebe [1, "a", 3] e retorna false', () => {
+    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     expect(numbers([1, 'a', 3])).toEqual(false);
-  });
 
-  it('Verifica se a função recebe [" "] e retorna false', () => {
+    // Escreva um teste em que a função recebe [' '] e retorna false
     expect(numbers([' '])).toEqual(false);
-  }); 
+  });
 });
