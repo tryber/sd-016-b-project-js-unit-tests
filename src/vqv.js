8c893912b1f6bf1b1a774f34c963209c9919bbad
lucaseix1 <lucas.bastos.seixas@gmail.com>
2021-09-29 17:50:52 -0300

    Questão-03/04

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  if (typeof (nome) === 'undefined' || typeof (idade) === 'undefined') {
+    return undefined;
+  }
+
+  let tex = `Oi, meu nome é ${nome}!\nTenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV!`;
+
+  return tex;
+};
 
 module.exports = vqv;
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,14 +25,19 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle('a')).toBeUndefined()
     // Teste se circle retorna um objeto.
+    expect(circle(1)).toMatchObject({radius: 1, area: 3.14, circumference: 6.28})
     // Teste se o objeto retornado tem 3 propriedades.
+    expect(circle('1')).toHaveproperty('radius','area','circumference')
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBeUndefined()
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    expect(circle(2)).toBeMatChObject({circumference: 12.56})
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    expect(circle(3).area).toBeCloseTo(28.26)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(3)).toBeMatChObject({radius: 3, circumference: 18.84})
   });
 });
