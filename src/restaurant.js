39f803c7547213503f995c72de5bd899db7f09a2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-29 17:41:21 -0300

    implement first feature restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,6 +79,12 @@
 // que percorre por todos os itens de `objetoRetornado.consumption`, soma o preço deles e retorna o valor somado acrescido de 10%.
 // DICA: para isso, você precisará percorrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = () => {
+  const menu = {
+    fetchMenu: () => {},
+  };
+
+  return menu;
+};
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -46,12 +46,12 @@ const createMenu = require('../src/restaurant');
 
 describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui a
     // chave fetchMenu, a qual tem como valor uma função.
-    // ```
-    // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
-    // ```
+    expect(typeof createMenu()).toBe('object');
+    expect(createMenu().fetchMenu).toBeDefined();
+    expect(typeof createMenu().fetchMenu).toBe('function');
+
     // TESTE 2: Verifique se 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`, 
     // considerando que a função createMenu() foi chamada com o objeto: `{ food: {}, drink: {} }`.
     // ```
