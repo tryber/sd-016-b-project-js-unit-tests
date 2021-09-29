9ffe7b934f45e9fbfbdf10fa4669d332187d9750
Matheus <matheusmoura2110@gmail.com>
2021-09-29 17:09:29 -0300

    começando exercicios

--- src/average.js
+++ src/average.js
@@ -1,18 +1,5 @@
-/*
-  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
-  Caso a função receba algum valor não númerico ou um array vazio,
-  o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
-  Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
-  Comportamento:
-    - average([2, 2]) // Retorno: 2;
-    - average([1, 1]) // Retorno: 1;
-    - average([1, '2']) // Retorno: undefined;
-*/
-
-const average = () => { };
+const media = (arrayOk) => Math.round(arrayOk.reduce((a, b) => a + b) / arrayOk.length);
+const average = (a) => (a.length && a.every((e) => typeof e === 'number') ? media(a) : undefined);
 
 module.exports = average;
-
+// Feito junto com o Arytheon da turma 16 tribo B
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,14 +25,45 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle('string')).toBeUndefined();
     // Teste se circle retorna um objeto.
+    expect(typeof circle(1)).toMatch('object');
     // Teste se o objeto retornado tem 3 propriedades.
+    expect(circle(7)).toMatch(Object.keys('objeto').length === 3);
+
+
+
+
+
+    const objetão = { radius: 1, area: 3.14, circumference: 6.28 }
+
+    console.log(objetão.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle(' ')).toBeUndefined();
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    expect(circle(2)).toBe('{circumference: 12.56 }')
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    expect(circle(3)).toContais('{circumference: 18.84}')
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(3)).toBe('{radius: 3, area: 28,26, circumference: 18.84}')
   });
 });
