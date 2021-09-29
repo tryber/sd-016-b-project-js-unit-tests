4ed42dd05078ff1038d03a78a0b3e4a36f486c43
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-29 19:16:26 -0300

    adiciona requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => ({
+  name: nome,
+  feedback: () => 'Eita pessoa boa!',
+});
 
+console.log(createStudent('Dani'));
 module.exports = createStudent;
