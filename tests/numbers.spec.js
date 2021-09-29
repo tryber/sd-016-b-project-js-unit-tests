e32cc840e9794027102fd0f7cfc4c6c4f7432e03
HugoDaniel2000 <hugodanielc67@gmail.com>
2021-09-29 16:13:48 -0300

    requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,6 +18,8 @@ describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     expect(numbers([1, 2, 3, 4, 5] )).toBe(true)
+  });
+  it('Verifica se a função `numbers`retorna `false` quando o array contém "string" ', () => {
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     expect(numbers([1, 2, '3', 4, 5])).toBe(false)
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
