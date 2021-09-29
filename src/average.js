ed7448d52859d8b312eae02262f3537d99210009
HiagoBriano <hiago.artist@hotmail.com>
2021-09-29 17:16:21 -0300

    requisito 1 feito

--- src/average.js
+++ src/average.js
@@ -1,9 +1,9 @@
+/* eslint-disable sonarjs/cognitive-complexity */
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
@@ -12,6 +12,27 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+// eslint-disable-next-line complexity
+const average = (array) => {
+  let somenteNumber = true;
+  let soma = 0;
+
+  // verifica se só tem numeros
+  for (let i = 0; i < array.length; i += 1) {
+    let validacao = typeof (array[i]) === 'number';
+
+    if (!validacao) {
+      somenteNumber = false;
+    }
+  }
+
+  if ((somenteNumber === true) && array.length > 0) { // verifica se é somente numero e se o array não é vazio
+    for (let i = 0; i < array.length; i += 1) { // soma os numeros do array
+      soma += array[i];
+    }
+    return (Math.round(soma / array.length));
+  } 
+    return undefined; // se não for somente numeros ou se o array for fazio vazio
+};
 
 module.exports = average;
