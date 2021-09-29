a4f4e14c2f857fe6e9ff607ebbedb68868d4419f
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-29 16:31:40 -0300

    adiciona requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -14,8 +14,19 @@
       Tenho 30 anos,
       trabalho na Trybe e mando muito em programação!
       #VQV!'
-*/
+*/ 
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (typeof name !== 'string' || typeof age !== 'number') {
+    return undefined;
+  }
+
+  let phrase = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  
+  return phrase;
+};
 
 module.exports = vqv;
