baeeba7027cd07e2b2c9bac77a47702cc3d8a347
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-29 17:14:07 -0300

    Req6: productDetails is a function

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,9 +28,9 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(productDetails).toBeInstanceOf(Function);
     // Teste se o retorno da função é um array.
     // Teste se o array retornado pela função contém dois itens dentro.
     // Teste se os dois itens dentro do array retornado pela função são objetos.
