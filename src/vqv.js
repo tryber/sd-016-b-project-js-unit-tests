fdec9ba65d6d3e2fd55553780af3b0d02597fbd7
MychaelJales <mychael_duarte@hotmail.com>
2021-09-30 20:42:29 -0300

    finalizando o requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  let msg;
+  if (name === undefined || age === undefined) {
+    msg = undefined;
+  } else {
+    msg = (`Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
+  }
+  return msg;
+};
 
 module.exports = vqv;
