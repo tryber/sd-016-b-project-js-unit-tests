55fbd937289416164cb8446bf90daf36f163a585
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-30 09:46:59 -0300

    trying again

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -14,17 +14,17 @@ const numbers = require('../src/numbers');
 
 */
 
-describe('2 - Implemente a função `numbers`', () => {
-  // it('Verifica se `retorna `true` quando o array contém [1, 2, 3, 4, 5]', () => {
-  //   expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
-  // });
-  // it('Verifica se `retorna `false` quando o array contém [1, 2, `3`, 4, 5]', () => {
-  //   expect(numbers([1, 2, '3', 4, 5])).toBeFalsy();
-  // });
-  // it('Verifica se `retorna `false` quando o array contém [1, `a`, 3]', () => {
-  //   expect(numbers([1, 'a', 3])).toBeFalsy();
-  // });
-  // it('Verifica se `retorna `false` quando o array contém [` `]', () => {
-  //   expect(numbers([' '])).toBeFalsy();
-  // });
+describe('2 - Implemente os casos de teste para a função `numbers`', () => {
+  it('Verifica se `retorna `true` quando o array contém [1, 2, 3, 4, 5]', () => {
+    expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
+  });
+  it('Verifica se `retorna `false` quando o array contém [1, 2, `3`, 4, 5]', () => {
+    expect(numbers([1, 2, '3', 4, 5])).toBeFalsy();
+  });
+  it('Verifica se `retorna `false` quando o array contém [1, `a`, 3]', () => {
+    expect(numbers([1, 'a', 3])).toBeFalsy();
+  });
+  it('Verifica se `retorna `false` quando o array contém [` `]', () => {
+    expect(numbers([' '])).toBeFalsy();
+  });
 });
