236e7fec5f10f57c52c824ad536ffaf9abbcf06a
tonistorres <sisvenda2011@gmail.com>
2021-09-29 23:47:38 -0300

    QT04 Respondida circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -24,15 +24,37 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
+  it('se o parametro passado na funcao não for um numero', () => {
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle()).toBeUndefined();
+    });
+    it('funcao cicle retorna uma objeto', () => {
     // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
+    expect(typeof circle(1)).toBe('object');
+    });
+     // Teste se o objeto retornado tem 3 propriedades.
+    it('O objeto retornado tem tres propriedades', () => {
+      // Teste se circle retorna um objeto.
+      expect(Object.keys(circle(2))).toHaveLength(3);
+    });
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    it('funcao nao recebe parametro retorne undefined', () => {
+      expect(circle()).toBeUndefined();
+    });
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    it('funcao retorna retorna dentro de um objeto a circuferência de raio 2', () => {
+      expect(circle(2).circumference).toBeCloseTo(12.56, 2);
+    });
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    it('funcao retorna retorna dentro de um objeto a area correta para circulo raio 3', () => {
+      expect(circle(3).area).toBeCloseTo(28.26, 2);
+    });
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-  });
+    it('Retorna um objeto com os dados corretos de um circulo de raio 3', () => {
+         expect(circle(3)).toMatchObject({
+        radius: 3,
+        circumference: 18.84,
+      });
+      expect(circle(3).area).toBeCloseTo(28.26, 2);
+    });
 });
