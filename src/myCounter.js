56cd55189cc01299372d154be03faef8eed5e0a5
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-30 11:18:25 -0300

    correction of the changes before done

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,16 +17,16 @@
 */
 
 const myCounter = () => {
-  // var myArray = [];
-  // for (var counter = 0; counter <= 3; counter += 1) {
-  //   myArray.push(counter);
-  //   for (var counter = 2; counter <= 3; counter += 1) {
-  //     myArray.push(counter);
-  //   }
-  // }
-  // return myArray;
-  const expectedOutput = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
-  return expectedOutput;
+  var myArray = [];
+  for (var counter = 0; counter <= 3; counter += 1) {
+    myArray.push(counter);
+    for (var counter = 2; counter <= 3; counter += 1) {
+      myArray.push(counter);
+    }
+  }
+  return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
