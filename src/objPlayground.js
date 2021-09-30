88793f6ffce564cb6c25e72a46e5c51e472295f9
Alissandra <alissandraraysa@gmail.com>
2021-09-30 17:31:19 -0300

    function calculator e generator

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -37,8 +37,8 @@ const calculator = (number1, number2) =>
   ({
     sum: (number1 + number2),
     mult: (number1 * number2),
-    div: Math.round(number1 / number2),
-    sub: Math.round(number1 - number2),
+    div: Math.floor(number1 / number2),
+    sub: Math.floor(number1 - number2),
   });
 
 const arrayGenerator = (type, object) => Object[type](object);
--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -42,14 +42,21 @@ Retorno:
 */
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
+  it('quando não recebe um paramtero retorna undefined', () => {
+  expect(getCharacter()).tobe(undefined)  
+  });
+  it('test se retorna o objeto correto para o paramatro "Arya"', () => {
+  expect(getCharacter()).toEqual({
+    name: 'Arya Stark',
+    class: 'Rogue',
+    phrases: ['Not today', 'A girl has no name.']
+  })  
+  });
+});
+// ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
     // Teste se os parâmetros não são Case Sensitive.
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
-  });
-});
