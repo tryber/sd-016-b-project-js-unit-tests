32354055b444e443d012934f793fe64aa11c097b
leandro <leandrochagask@gmail.com>
2021-09-29 21:59:32 -0300

    Requisito 07

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,19 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const result = {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.trunc(number1 / number2),
+    sub: number1 - number2
+  }
 
-const arrayGenerator = (type, object) => {};
+  return result
+};
+
+const arrayGenerator = (type, object) => {
+  return result = Object[type](object);
+};
 
 module.exports = { calculator, arrayGenerator };
