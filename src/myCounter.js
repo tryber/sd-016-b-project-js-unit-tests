3151793548b0f1ab84563a90f26af41cc81b458e
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-30 20:19:15 -0300

    Add requirement 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,11 +17,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  const myArray = [];
+  for (let count = 0; count <= 3; count += 1) {
+    myArray.push(count);
+    for (let count2 = 2; count2 <= 3; count2 += 1) {
+      myArray.push(count2);
     }
   }
   return myArray;
