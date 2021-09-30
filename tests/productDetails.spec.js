7e6a42c2d55f8ab13d34c2c017822af9c7c72702
Mariana Saraiva <marianascm8@gmail.com>
2021-09-29 23:37:59 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,15 +27,28 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    // ESCREVA SEUS TESTES ABAIXO:
+  it('Teste se productDetails é uma função.', () => {
     // Teste se productDetails é uma função.
     expect(typeof productDetails).toBe('function');
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    expect(productDetails({}).length).toEqual(5);
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  });
+  it('Teste se o retorno da função é um array.', () => {
+    expect(productDetails('Alcool gel', 'Máscara')).toBeInstanceOf(Array);
+  });  
+  it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+    // Matcher para verificar o tamanho.
+    expect(productDetails('Alcool gel', 'Máscara')).toHaveLength(2);
+  });
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    expect(productDetails('Alcool gel', 'Máscara')[0]).toBeInstanceOf(Object);
+    expect(productDetails('Alcool gel', 'Máscara')[1]).toBeInstanceOf(Object);
+  });
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    const [objeto, objeto2] = productDetails('a', 'b');
+    expect(objeto).not.toEqual(objeto2);
+  });
+  it('Teste se os dois productIds terminam com 123.', () => {
+    const [objeto, objeto2] = productDetails('a', 'b');
+    expect(objeto.details.productId.endsWith('123')).toBe(true);
+    expect(objeto2.details.productId.endsWith('123')).toBe(true);
   });
 });
