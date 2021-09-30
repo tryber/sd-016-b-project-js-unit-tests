5737d90de4d2e99cd67090827015e916d80f458a
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-09-30 15:15:44 -0300

    Conclui requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let obj = new Object();
+  obj['name'] = name;
+  obj.feedback = () => {
+    return 'Eita pessoa boa!';
+  }
+  return obj;
+};
 
 module.exports = createStudent;
