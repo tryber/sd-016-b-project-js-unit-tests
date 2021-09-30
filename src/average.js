d05292f0c10bf98eb8a9a292349deba0494d539d
antonio_carlos <acoliveira95@gmail.com>
2021-09-30 18:13:15 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here 
+const average = (array) => {
+  let n = 0; // add your implementation here
+    if (array.length === 0) {
+      return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof (array[i]) !== 'number') {
+      return undefined;
+    }
+    n += array[i];
+  }
+  return Math.round(n / array.length);
 };
 
 module.exports = average;
