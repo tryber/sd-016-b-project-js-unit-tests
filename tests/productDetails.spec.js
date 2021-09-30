3c918fb888dd670fcab30432416a688f9a785a6e
Izabela <izabelahammer@gmail.com>
2021-09-30 11:49:48 -0300

    requisito 6 concluido

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,15 +31,17 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se productDetails é uma função.
     expect(typeof(productDetails)).toBe('function');
     // Teste se o retorno da função é um array.
-    expect(Array.isArray(productDetails())).toBe(true);
+    expect(Array.isArray(productDetails() ) ).toBe(true);
     // Teste se o array retornado pela função contém dois itens dentro.
     expect(productDetails('sorvete', 'chocolate').length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // problema expect(typeof(productDetails('sorvete', 'chocolate')[0]).toBe('Object');
-    //problema expect(typeof(productDetails('sorvete', 'chocolate')[1]).toBe('Object');
+    expect( typeof( productDetails('sorvete', 'chocolate')[0] )).toBe('object');
+    expect( typeof( productDetails('sorvete', 'chocolate')[1] )).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     expect(productDetails('sorvete', 'chocolate')[0] !== productDetails('sorvete', 'chocolate')[1]).toBe(true);
     // Teste se os dois productIds terminam com 123.
-    //problema expect(productDetails('sorvete', 'chocolate')).toContain('123');
+    //fonte: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+    expect(productDetails('sorvete', 'chocolate')[0].details.productId.slice(-3)).toMatch(/123/);
+    expect(productDetails('sorvete', 'chocolate')[1].details.productId.slice(-3)).toMatch(/123/);
   });
 });
