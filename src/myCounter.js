f7b1de6e1049bc15c79652bf5d9a04571c8de151
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-10-01 19:56:26 -0300

    resolve o requisito 8 (agora vai)

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,11 +17,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
+  let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let count = 2; count <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let count = 2; count <= 3; count += 1) {
+      myArray.push(count);
     }
   }
   return myArray;
