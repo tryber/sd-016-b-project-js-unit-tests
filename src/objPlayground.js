7d7eac0617231bf7ccc9eef9f569f8d39dbb16cf
MychaelJales <mychael_duarte@hotmail.com>
2021-10-01 17:19:45 -0300

    finalizando o requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,33 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const entries = (object) => {
+  let resultEntries = [];
+  for (let index = 0; index < Object.keys(object).length; index += 1) {
+    resultEntries.push([Object.keys(object)[index], Object.values(object)[index]]);
+  }
+  return resultEntries;
+};
 
-const arrayGenerator = (type, object) => {};
+const calculator = (number1, number2) => {
+  const calc = { sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  };
+  return calc;
+};
+
+const arrayGenerator = (type, object) => {
+  let result;
+  if (type === 'keys') {
+    result = Object.keys(object); 
+  } else if (type === 'values') {
+    result = Object.values(object);
+  } else {
+    result = entries(object);
+  }
+  return result;
+};
 
 module.exports = { calculator, arrayGenerator };
