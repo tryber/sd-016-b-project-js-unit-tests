dbb614b82096abb9172594159efe282bb0a08614
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-10-01 19:30:11 -0300

    resolve o requisito 8.

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,9 +18,9 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
@@ -28,3 +28,5 @@ const myCounter = () => {
 };
 
 module.exports = myCounter;
+
+console.log(myCounter());
