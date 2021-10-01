02ae8e1dafd8f31b8279bb6c960b63c583afd19e
Borba <luisfpb@icloud.com>
2021-10-01 14:18:12 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,15 @@
 */
 
 const vqv = (name, age) => {
-  const text = `Oi, meu nome é ` + name + `!`
-  `Tenho ` + age + ` anos,`
-  `trabalho na Trybe e mando muito em programação!`
-  `#VQV!`
+
+  if (name === undefined || age === undefined) {
+    return undefined;
+  }
+
+return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
