8be41d2c266961175db242e2603b414d81fc7fe9
Renata <renatamagno@live.com>
2021-10-01 10:57:25 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (name !== undefined && age !== undefined) {
+  const paragraph = `Oi, meu nome é ${name}!
+  Tenho ${age} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+  return paragraph;
+  }
+  return undefined;
+};
 
 module.exports = vqv;
