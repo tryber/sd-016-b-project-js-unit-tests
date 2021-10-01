644b33bfe2bf57738759ac1db1cd5f5502f2319c
Douglas Toledo <Douglas Toledo - Duskimm>
2021-10-01 13:04:50 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof(productDetails)).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails())).toBeTruthy();
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('Alcool gel123', 'Máscara123')).toHaveLength(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    const products = productDetails('Alcool gel123', 'Máscara123');
+    for(let index in products) {
+      expect(typeof products[index]).toBe('object');
+    }
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(products[0]).not.toEqual(products[1]);
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('Alcool gel123', 'Máscara123')[0].details.productId.slice(-3)).toMatch(/123/);
+    expect(productDetails('Alcool gel123', 'Máscara123')[1].details.productId.slice(-3)).toMatch(/123/);
   });
 });
