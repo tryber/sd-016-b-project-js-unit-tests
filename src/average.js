b93548c47cf105b745a75fa38541ab5cf9a08ec0
Matheus <matheusmoura2110@gmail.com>
2021-09-30 22:50:27 -0300

    task 6

--- src/average.js
+++ src/average.js
@@ -1,5 +1,7 @@
 const media = (arrayOk) => Math.round(arrayOk.reduce((a, b) => a + b) / arrayOk.length);
 const average = (a) => (a.length && a.every((e) => typeof e === 'number') ? media(a) : undefined);
 
+console.log(average(8));
+
 module.exports = average;
 // Feito junto com o Arytheon da turma 16 tribo B
