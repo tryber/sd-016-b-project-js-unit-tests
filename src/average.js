bcc5b850090b148e0811a0c1d3c762119da44290
Joey <joviavelino@gmail.com>
2021-10-02 20:54:54 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -14,16 +14,16 @@
 const average = (array) => {
   let sum = 0;
     if (array.length === 0) {
-      return undefined
+      return undefined;
     }
-  for(let index = 0; index < array.length; index++) {
+  for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') {
-      return undefined 
+      return undefined;
       }
-    sum += array[index]
+    sum += array[index];
     }
     
-   return Math.round(sum / (array.length))
-}
+   return Math.round(sum / (array.length));
+};
 
 module.exports = average;
