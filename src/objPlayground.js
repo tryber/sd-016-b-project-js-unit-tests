ea2729c3e5cb9083c01010e9b57d4ec896ebac2e
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-10-03 08:39:08 -0300

    requisito 8 feito

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,30 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const calculadoraObj = {
+    sum: Math.trunc(number1 + number2),
+    mult: Math.trunc(number1 * number2),
+    div: Math.trunc(number1 / number2),
+    sub: Math.trunc(number1 - number2),
+  };
+  return calculadoraObj;
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') {
+    return Object.keys(object);
+  }
+
+  if (type === 'values') {
+    return Object.values(object);
+  }
+
+  if (type === 'entries') {
+    return Object.entries(object);
+  }
+};
+
+console.log(arrayGenerator('entries', calculator(8, 2)));
 
 module.exports = { calculator, arrayGenerator };
