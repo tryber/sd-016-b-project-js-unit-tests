b94ee2f0ba6c5fa4d92664301e90cc579d628c79
Douglas Sabino <douglasbsabino@hotmail.com>
2021-10-03 14:44:47 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,28 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+let arr = [5,5,5,5]
+
+const average = (arr) => {
+  let media = 0
+
+  for(let i = 0; i < arr.length; i++) {
+    if(typeof arr[i] !== 'number') return undefined
+   }
+
+  for(let i = 0; i <= arr.length; i++) {
+    if(arr.length === 0) return undefined
+   }
+
+  for(let i = 0; i < arr.length; i++){
+    media = media + arr[i]
+  }
+
+  media = media / arr.length
+
+  return Math.round(media)
+};
+
+console.log(average(arr));
 
 module.exports = average;
