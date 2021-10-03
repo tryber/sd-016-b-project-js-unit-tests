5f8c8f39728c96254add9f216b986dfeb39df1cf
Renata <renatamagno@live.com>
2021-10-03 20:25:43 -0300

    Requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,26 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => (
+  {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  }
+);
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  switch (type) {
+    case 'keys':
+      return (Object.keys(object));
+    case 'values':
+      return (Object.values(object));
+    case 'entries':
+      return (Object.entries(object));
+    default:
+      // no default
+  }
+};
 
 module.exports = { calculator, arrayGenerator };
