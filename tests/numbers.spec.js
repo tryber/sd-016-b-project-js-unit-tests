f1b5dc83a1b9332bc2ed22d16c6386302248d3a1
claudinei <claudineigraunki@hotmail.com>
2021-10-03 21:27:14 -0300

    segundo requisito projeto.

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -21,5 +21,9 @@ describe('2 - Implemente os casos de teste para a função `numbers`', () => {
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers([1, 2, 3, 4, 5])).toBeTruthy()
+    expect(numbers([1, 2, '3', 4, 5])).toBeFalsy()
+    expect(numbers([1, 'a', 3])).toBeFalsy()
+    expect(numbers([' '])).toBeFalsy()
   });
 });
