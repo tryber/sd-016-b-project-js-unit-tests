405c133891ce604aa148194a60b8c1376799bbd2
Douglas Sabino <douglasbsabino@hotmail.com>
2021-10-04 02:26:17 -0300

    lint requisito myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,11 +20,13 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let count = 2; counter <= 3; counter += 1) {
-      myArray.push(count);
+    for (let counter = 2; counter <= 3; counter += 1) {
+      myArray.push(counter);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
