dbfe2a0a8bc5866a96e5dbae269ba685471e53f6
Savio Silva <s.drksilva@gmail.com>
2021-10-04 14:43:07 -0300

    Finalized exercise vqv - Refactored

--- src/vqv.js
+++ src/vqv.js
@@ -18,8 +18,7 @@
 
 const vqv = (name, number) => {
   if (name === undefined && number === undefined) return undefined;
-  return `
-Oi, meu nome é ${name}!
+  return `Oi, meu nome é ${name}!
 Tenho ${number} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
