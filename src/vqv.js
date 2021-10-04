fa05937138c7f5b42ae81454d70a6ad5ce930cd7
natashiaromanzini <natashiaromanzini1@gmail.com>
2021-10-04 08:23:03 -0300

    resultados

--- src/vqv.js
+++ src/vqv.js
@@ -1,21 +1,14 @@
-/* eslint-disable no-unused-vars */
-
-/*
-  Use template literals para escrever uma função que,
-  recebe seu nome e sua idade e retorna o parágrafo descrito abaixo.
-  Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
-
-  Parâmetros:
-    - Uma string;
-    - Um número.
-  Comportamento:
-    vqv(Tunico, 30) // Retorna:
-      'Oi, meu nome é Tunico!
-      Tenho 30 anos,
-      trabalho na Trybe e mando muito em programação!
-      #VQV!'
-*/
-
 const vqv = () => {};
+const vqv = (name, age) => {
+  if (typeof name !== 'string' || typeof age !== 'number') {
+    return undefined;
+  }
 
+  let phrase = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return phrase;
+};
 module.exports = vqv;
--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,19 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable no-unused-vars */
-
-const numbers = require('../src/numbers');
-
-/*
-  A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
-
-  Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
-  Comportamento:
-    - numbers([2, 3, 4]); // Retorna: true
-    - numbers([2, 'errado', 5]); // Retorna: false
-
-*/
-
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
     fail('Teste vazio!');
@@ -21,5 +5,20 @@ describe('2 - Implemente os casos de teste para a função `numbers`', () => {
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     // Escreva um teste em que a função recebe [' '] e retorna false
+  // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros', () => {
+    expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
+   }); 
+  // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+  it('Verifica se a função `numbers`retorna `false` quando o array contém parametros NaN', () => {
+    expect(numbers([1, 2, '3', 4, 5])).toEqual(false);
+  });
+  // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+  it('Verifica se a função `numbers`retorna `false` quando o array contém letras', () => {
+    expect(numbers([1, 'a', 3])).toEqual(false);
+  });
+  // Escreva um teste em que a função recebe [' '] e retorna false
+  it('Verifica se a função `numbers`retorna `false` quando o array é vazio', () => {
+    expect(numbers([' '])).toEqual(false);
   });
 });
