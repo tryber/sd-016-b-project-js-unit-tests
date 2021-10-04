b08533535cc5c3137726ceabfc84265d223e89d7
Savio Silva <s.drksilva@gmail.com>
2021-10-04 11:29:59 -0300

    Finalized exercise myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-var */
-/* eslint-disable block-scoped-var */
+/* eslint-disable no-let */
+/* eslint-disable block-scoped-let */
 /* eslint-disable no-redeclare */
-/* eslint-disable vars-on-top */
+/* eslint-disable lets-on-top */
 
 /*
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
@@ -17,11 +17,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  let myArray = [];
+  for (let k = 0; k <= 3; k += 1) {
+    myArray.push(k);
+    for (let j = 2; j <= 3; j += 1) {
+      myArray.push(j);
     }
   }
   return myArray;
