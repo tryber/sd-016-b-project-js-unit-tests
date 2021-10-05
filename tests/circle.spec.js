a7eecf30725f5112aa2622ec5980828a2613eb7c
Hugo Maximiano <hugomaximiano@hotmail.com>
2021-10-05 20:09:47 -0300

    myCounter

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -38,7 +38,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     expect(Object.values(circle(2))[2]).toEqual(12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     expect(Object.values(circle(3))[1]).toBeCloseTo(28.26);
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3. 
     expect((circle(3))).toEqual({ radius: 3, area: 28.259999999999998, circumference: 18.84 });
   });
 });
