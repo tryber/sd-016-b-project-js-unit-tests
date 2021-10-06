b42b46fc5e98bbf2a16c7d71dd741928906defb3
Marcus Thadeu <marcusthadeu@outlook.com>
2021-10-06 11:24:51 -0300

    9 Done

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -46,10 +46,32 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
     fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBeUndefined();
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rouge',
+      phrases: ['not today', 'A girl has no name.']
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre')).toEqual({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.']
+    });
     // Teste se os parâmetros não são Case Sensitive.
+    expect(getCharacter('Brienne')).toMatchObject({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']
+    });
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('John')).toBeUndefined();
   });
 });
