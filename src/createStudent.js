c2da7f8e768141a42dace07dca090fb6ab289584
AmandaSottani <amandasottani@gmail.com>
2021-10-06 15:05:22 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (user) => ({
+  name: user,
+  feedback: () => 'Eita pessoa boa!',
+});
+
 
 module.exports = createStudent;
