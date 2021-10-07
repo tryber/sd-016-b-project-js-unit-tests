8b74c7dc7f9d12473c6612c113f88d715e5e27cf
Pedro Figueiredo <pedrofigueiredo25@outlook.com>
2021-10-07 14:00:46 -0300

    Adiciona arquivos src projeto ja feito

--- src/myCounter.js
+++ src/myCounter.js
@@ -7,7 +7,6 @@
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
   Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
   Corrija o código abaixo para que a função retorne o array correto.
-  Atenção: não remova os `for`s da função e sim corrija os erros presentes.
 
   Parâmetros:
   - Nenhum.
@@ -16,15 +15,19 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
+// Quebrei a cabeça procurando erro na logica e o erro tava no escopo :clown:
+
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  let myArray = [];
+  for (let index = 0; index <= 3; index += 1) {
+    myArray.push(index);
+    for (let index2 = 2; index2 <= 3; index2 += 1) {
+      myArray.push(index2);
     }
   }
   return myArray;
 };
+
+console.log(myCounter);
 
 module.exports = myCounter;
