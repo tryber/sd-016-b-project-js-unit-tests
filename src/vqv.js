483722472d65dcd3b5b7f82a71ecc7c580242f17
Thiago Silva <thiago.developer.rj@gmail.com>
2021-10-21 12:51:52 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined && idade === undefined) {
+    return undefined;
+  }
+  let paragrafo;
+  paragrafo = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return paragrafo;
+};
 
 module.exports = vqv;
